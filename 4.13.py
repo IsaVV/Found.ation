afb9b0c3d1fd51518a2b8bb36e7cfafbdbccd0ee
Isadora Vasconcelos Viana <64534791+IsaVV@users.noreply.github.com>
2020-09-30 17:07:32 -0300

    Delete 4.13.py

--- 4.13.py
+++
@@ -1,7 +0,0 @@
-palavra = input("Digite uma palavra: ")
-if palavra == palavra[::-1]:
-    print("A palavra digitada é um palíndromo")
-else:
-    print("A palavra digitada não é um palíndromo")
-    print(palavra)
-    print(palavra[::-1])
