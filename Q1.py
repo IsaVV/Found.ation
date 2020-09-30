7917dd4b5c2f7264ee7d5df201dccda6c746ddd7
Isadora Vasconcelos Viana <64534791+IsaVV@users.noreply.github.com>
2020-09-30 17:11:12 -0300

    Delete Q1.py

--- Q1.py
+++
@@ -1,9 +0,0 @@
-num = int(input('digite um número: \n'))
-if num % 2 == 0:
-    print('Par')
-elif num % 2 != 0:
-    print('Ímpar')
-if num < 0:
-    print('Negativo')
-else:
-    print('Positivo')
