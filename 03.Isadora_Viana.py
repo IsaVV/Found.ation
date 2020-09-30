d522eba272a0982e4c3206fcf566a429db2d79b2
Isadora Vasconcelos Viana <64534791+IsaVV@users.noreply.github.com>
2020-09-30 17:06:45 -0300

    Delete 03.Isadora_Viana.py

--- 03.Isadora_Viana.py
+++
@@ -1,9 +0,0 @@
-
-pessoas = ([],[],[],[],[])
-while (len(pessoas[0])) != 5:
-    a = input('Nome: ')
-    b = (float(input('Peso: ')))
-    if b >= 50:
-        pessoas[0].append(a)
-        pessoas[1].append(b)
-print(pessoas)
